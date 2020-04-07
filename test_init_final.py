92721d389089ebc86fecbfe6f427041b91b70f62
ilsang1212 <58781790+ilsang1212@users.noreply.github.com>
2020-04-08 04:50:52 +0900

    Add files via upload

--- test_init_final.py
+++ test_init_final.py
@@ -1469,6 +1469,12 @@ while True:
 		else:
 			return
 
+	@client.event
+	async def on_command_error(ctx, error):
+		if isinstance(error, CommandNotFound):
+			return
+		raise error
+
 	# 봇이 새로운 메시지를 수신했을때 동작되는 코드입니다.
 	@client.event
 	async def on_message(msg):
