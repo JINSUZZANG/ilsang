ec863e8109a1f83bbd566e00d791faa3228907f0
ilsang1212 <58781790+ilsang1212@users.noreply.github.com>
2020-04-09 21:31:37 +0900

    Add files via upload

--- test_init_final.py
+++ test_init_final.py
@@ -1371,18 +1371,32 @@ while True:
 			racing_result = []
 			output = ':camera: :camera: :camera: 신나는 레이싱! :camera: :camera: :camera:\n'
 			#racing_unit = [':giraffe:', ':elephant:', ':tiger2:', ':hippopotamus:', ':crocodile:',':leopard:',':ox:', ':sheep:', ':pig2:',':dromedary_camel:',':dragon:',':rabbit2:'] #동물스킨
-			racing_unit = [':red_car:', ':taxi:', ':bus:', ':trolleybus:', ':race_car:', ':police_car:', ':ambulance:', ':fire_engine:', ':minibus:', ':truck:', ':articulated_lorry:', ':tractor:', ':scooter:', ':manual_wheelchair:', ':motor_scooter:', ':auto_rickshaw:', ':blue_car:', ':bike:', ':helicopter:', ':steam_locomotive:']  #탈것스킨
-			random.shuffle(racing_unit) 
+			#racing_unit = [':red_car:', ':taxi:', ':bus:', ':trolleybus:', ':race_car:', ':police_car:', ':ambulance:', ':fire_engine:', ':minibus:', ':truck:', ':articulated_lorry:', ':tractor:', ':scooter:', ':manual_wheelchair:', ':motor_scooter:', ':auto_rickshaw:', ':blue_car:', ':bike:', ':helicopter:', ':steam_locomotive:']  #탈것스킨
+			#random.shuffle(racing_unit) 
 			racing_member = msg.split(" ")
 
+			racing_unit = []
+
+			emoji = discord.Emoji
+			emoji = ctx.message.guild.emojis
+
+			for j in range(len(tmp_racing_unit)):
+				racing_unit.append(':' + tmp_racing_unit[j] + ':')
+				for i in range(len(emoji)):
+					if emoji[i].name == tmp_racing_unit[j].strip(":"):
+						racing_unit[j] = '<:' + tmp_racing_unit[j] + ':' + str(emoji[i].id) + '>'
+
+			random.shuffle(racing_unit)
+
 			if len(racing_member) == 1:
-				await ctx.send('```레이스 인원이 1명 입니다.```')
+				await ctx.send('레이스 인원이 1명 입니다.')
 				return
 			elif len(racing_member) >= 13:
-				await ctx.send('```레이스 인원이 12명 초과입니다.```')
+				await ctx.send('레이스 인원이 12명 초과입니다.')
 				return
 			else :
 				race_val = random.sample(range(14, 14+len(racing_member)), len(racing_member))
+				random.shuffle(race_val)
 				for i in range(len(racing_member)):
 					fr.append(racing_member[i])
 					fr.append(racing_unit[i])
@@ -1393,18 +1407,17 @@ while True:
 						fr.append(" ")
 					racing_field.append(fr)
 					fr = []
-
+	
 				for i in range(len(racing_member)):
 					racing_field[i][0] = "|"
 					racing_field[i][64] = race_info[i][1]
 					racing_field[i][65] = "| " + race_info[i][0]
 					str_racing_field.append("".join(racing_field[i]))
 					cur_pos.append(64)
-
+				
 				for i in range(len(racing_member)):
 					output +=  str_racing_field[i] + '\n'
-					
-				
+
 				result_race = await ctx.send(output + ':traffic_light: 3초 후 경주가 시작됩니다!')
 				await asyncio.sleep(1)
 				await result_race.edit(content = output + ':traffic_light: 2초 후 경주가 시작됩니다!')
@@ -1420,7 +1433,7 @@ while True:
 					test.append(1)
 					test.sort(reverse=True)
 					random_pos.append(test)
-				
+
 				for j in range(len(random_pos[0])):
 					if j%2 == 0:
 						output =  ':camera: :camera_with_flash: :camera: 신나는 레이싱! :camera_with_flash: :camera: :camera_with_flash:\n'
@@ -1470,9 +1483,7 @@ while True:
 						result[i][1] = ':nine:'
 					elif result[i][1] == 10:
 						result[i][1] = ':keycap_ten:'
-					elif result[i][1] == 11:
-						result[i][1] = ':x:'
-					elif result[i][1] == 12:
+					else:
 						result[i][1] = ':x:'
 					result_str += result[i][1] + "  " + result[i][0] + "  "
 					
@@ -1491,6 +1502,7 @@ while True:
 	# 봇이 새로운 메시지를 수신했을때 동작되는 코드입니다.
 	@client.event
 	async def on_message(msg):
+		await client.wait_until_ready()
 		if msg.author.bot: #만약 메시지를 보낸사람이 봇일 경우에는
 			return None #동작하지 않고 무시합니다.
 
